6f19dd7e9cf7a94391d99421b8239e73804b3b02
Alexei Bushuev <alexeib.2017@gmail.com>
2017-09-25 17:14:34 +0300

    add Person list api

--- backend/contacts/tests.py
+++ backend/contacts/tests.py
@@ -2,5 +2,19 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.urls import reverse
+from .models import Person
 
-# Create your tests here.
+
+class PersonIndexTests(TestCase):
+    def test_no_persons(self):
+        response = self.client.get(reverse('contacts:persons_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, '{"status": "Ok", "table": []}')
+
+    def test_with_person(self):
+        Person.objects.create(first_name='Alexei', last_name='Bushuev')
+        response = self.client.get(reverse('contacts:persons_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, '{"status": "Ok", "table": [{"comment": "",'
+                                           ' "first_name": "Alexei", "last_name": "Bushuev", "middle_name": ""}]}')
--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -4,7 +4,30 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import json
+from .models import Person
+
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Hello, World!')
+
+def persons_list(request):
+    """
+    List of persons
+    :param request:
+    :return: json list of persons
+    """
+    persons = Person.objects.order_by('first_name')
+
+    table = []
+    for person in persons:
+        rec = {}
+        rec['first_name']  = person.first_name
+        rec['middle_name'] = person.middle_name
+        rec['last_name']   = person.last_name
+        rec['comment']     = person.comment
+        table.append(rec)
+
+    result = {'status': 'Ok',
+               'table': table}
+
+    return HttpResponse(json.dumps(result), content_type='application/json')
