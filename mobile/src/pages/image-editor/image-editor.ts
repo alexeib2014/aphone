1f2894fdb9b9e703539c7bb3fe520314c52875e3
Alexei Bushuev <alexeib.2017@gmail.com>
2018-01-07 13:25:07 +0300

    ImageEditor : add buttons

--- mobile/src/pages/image-editor/image-editor.ts
+++ mobile/src/pages/image-editor/image-editor.ts
@@ -9,8 +9,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ImageEditorPage {
 
-  @ViewChild("viewPort")
-  viewPort: ElementRef;
+  @ViewChild("controlBoard") controlBoard: ElementRef;  
+  @ViewChild("viewMask") viewMask: ElementRef;
+  @ViewChild("viewPort")  viewPort: ElementRef;
+  @ViewChild("imagePort") imagePort: ElementRef;
+
+  winX : number;
+  winY : number;
+  winH : number;
+  winW : number;
+  winC : number;
+  winM : number;
 
   vpoX : number;
   vpoY : number;
@@ -24,6 +33,18 @@ export class ImageEditorPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ImageEditorPage');
+  }
+
+  ionViewDidEnter() {
+    let controlBoard = this.controlBoard.nativeElement;
+    let viewMask = this.viewMask.nativeElement;
+    let imagePort = this.imagePort.nativeElement;
+
+    this.winW = controlBoard.clientWidth;
+    this.winH = controlBoard.clientHeight;
+
+    viewMask.style.width = this.winW + 'px';
+    viewMask.style.height = this.winH + 'px';
 
     this.vpoH = this.vpoW = this.viewPort.nativeElement.clientWidth;
     this.viewPort.nativeElement.width = this.vpoW;
@@ -33,5 +54,8 @@ export class ImageEditorPage {
     this.vpoC = Math.ceil(this.vpoX + ( this.vpoW / 2 ));
     this.vpoM = Math.ceil(this.vpoY + ( this.vpoH / 2 ));
     console.log({ vpoX:this.vpoX, vpoY:this.vpoY, vpoW:this.vpoW, vpoH:this.vpoH, vpoC:this.vpoC, vpoM:this.vpoM })
+
+    imagePort.style.width = this.winW + 'px';
+    imagePort.style.height = this.winH + 'px';
   }
 }
